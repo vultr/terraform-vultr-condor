e4e60df631b484dafbe793f98bc50c0a41909e76
oogy <tylerweldon94@gmail.com>
2020-05-13 11:07:15 -0400

    syntax fix and main_ip -> id

--- vultr-external-lb.tf
+++ vultr-external-lb.tf
@@ -3,12 +3,12 @@ resource "vultr_load_balancer" "external_lb" {
 
   region_id = data.vultr_region.cluster_region.id
   label = "${var.cluster_name}-external-lb"
-  attached_instances = vultr_server.controllers.*.main_ip
+  attached_instances = vultr_server.controllers.*.id
   
   forwarding_rules {
     frontend_protocol = "tcp"
     frontend_port = var.external_lb_frontend_port
     backend_protocol = "tcp"
-    backend_port = var.external_lb_backend_port
+    backend_port = var.external_lb_frontend_port
   }
 }
