504de281f903f724993238679b2b8750b5117240
Tyler Weldon <tylerweldon94@gmail.com>
2020-05-20 14:44:50 -0400

    Yamls

--- vultr-controllers.tf
+++ vultr-controllers.tf
@@ -100,10 +100,48 @@ resource "null_resource" "cluster_init_ha" {
       "set -euxo", 
 #      "kubeadm init --control-plane-endpoint=${var.kube_api_dns_subdomain}.${var.cluster_domain}:${var.external_lb_frontend_port} --upload-certs --pod-network-cidr=${var.pod_network_cidr} 2>&1 | tee /tmp/cluster_init.log", 
       "kubeadm init --control-plane-endpoint=${var.kube_api_dns_subdomain}.${var.cluster_domain}:${var.external_lb_frontend_port} --upload-certs --pod-network-cidr=${var.pod_network_cidr}",
+      "mkdir -p $HOME/.kube", 
+      "sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+      "sudo chown $(id -u):$(id -g) $HOME/.kube/config",
 #      "cat /tmp/cluster_init.log | tr -d '\n' | tr -d '\\' | grep -Po \"kubeadm join ${var.kube_api_dns_subdomain}.${var.cluster_domain}:${var.external_lb_frontend_port} --token [a-zA-Z0-9]{6}.[a-zA-Z0-9]{16}      --discovery-token-ca-cert-hash sha256:[a-zA-Z0-9]{64}      --control-plane --certificate-key [a-zA-Z0-9]{64}\" > /tmp/controller-join-command",
       "mkdir ~/vultr",
     ]
   }  
+
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_server.controllers[0].main_ip}:~/.kube/config admin-${terraform.workspace}.config"
+  }
+
+  provisioner "file" {
+    content     = templatefile("${path.module}/templates/vultr/ccm-api-key.yml.tpl", { CLUSTER_API_KEY = var.cluster_api_key, CLUSTER_REGION = data.vultr_region.cluster_region.id })
+    destination = "~/vultr/ccm-api-key.yml"
+  }
+
+  provisioner "file" {
+    content     = templatefile("${path.module}/templates/vultr/csi-api-key.yml.tpl", { CLUSTER_API_KEY = var.cluster_api_key })
+    destination = "~/vultr/csi-api-key.yml"
+  }
+
+  provisioner "file" {
+    content     = data.http.vultr_ccm_file.body
+    destination = "~/vultr/vultr-ccm.yml"
+  }
+
+  provisioner "file" {
+    content     = data.http.vultr_csi_file.body
+    destination = "~/vultr/vultr-csi.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "kubectl apply -f ${var.cluster_cni}",
+      "kubectl apply -f ~/vultr/ccm-api-key.yml",
+      "kubectl apply -f ~/vultr/csi-api-key.yml",
+      "kubectl apply -f ~/vultr/vultr-ccm.yml",
+      "kubectl apply -f ~/vultr/vultr-csi.yml",
+    ]
+  }
+
 
   provisioner "local-exec" {
     command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${vultr_server.controllers[0].main_ip}:/tmp/controller-join-command ${path.module}/scripts/controller/remote/controller-join"
