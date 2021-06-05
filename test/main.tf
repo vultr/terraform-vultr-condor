83457e697c19dc0ff9ee6947fd49f1816b181842
oogy <tylerweldon94@gmail.com>
2021-06-05 14:37:32 -0400

    fmt

--- test/main.tf
+++ test/main.tf
@@ -1,5 +1,5 @@
 module "k0s" {
   source                 = "../"
   provisioner_public_key = chomp(file("~/.ssh/id_rsa.pub"))
-  cluster_vultr_api_key = var.cluster_vultr_api_key
+  cluster_vultr_api_key  = var.cluster_vultr_api_key
 }
